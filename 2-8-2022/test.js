ee67956aec15771764aee1a42f38a7d091e82191
Adham <adhambarakat727@gmail.com>
2022-08-02 15:06:58 +0200

    first commit

---
+++ 2-8-2022/test.js
@@ -0,0 +1,70 @@
+let arr = [40, 30, 20, 10]
+
+/*
+        // i=4, j=i
+        [40, 30, 20, 10]
+        [30, 40, 20, 10]
+        [30, 20, 40, 10]
+        [30, 20, 10, 40]
+        
+        i=3, j=i
+        [30, 20, 10, 40]
+        [20, 30, 10, 40]
+        [20, 10, 30, 40]
+        
+        [20, 10, 30, 40]
+        [10, 20, 30, 40]
+
+        [10, 20, 30, 40]
+
+*/
+
+
+
+let flag = "Not Found";
+
+let c = 'adham'
+
+arr.push(c)
+
+// arr.sort(function(a, b){return a - b})
+// arr.reverse()
+
+// for ( let x of arr ){
+
+//     console.log(x);
+
+// }
+
+
+// function Sort(arr){
+//     let Arr =arr
+//     for (let i= Arr.length; i > -1 ; i--){
+
+//         for (let j = i; j > -1 ; j--){
+//             if( Arr[j-1] < Arr[j-2]){
+//                 [Arr[j-2], Arr[j-1]] = [Arr[j-1], Arr[j-2]] 
+//             }
+//         }
+
+//     }
+//     return Arr
+// }
+// arr=Sort(arr)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+// function tt(){
+//     console.log('Bassem')
+// }
+
+// tt()
+// tt()
+// tt()
+
+// func= ()=>{
+//     console.log('Hello')
+// }
+
+// func()
